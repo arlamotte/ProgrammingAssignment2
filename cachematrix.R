a1fd4b13955f8f0e8520c44c3a1c00f097bb0ce4
arlamotte <alamotte@exacttarget.com>
2014-12-18 15:20:32 -0200

    Week 3 functions v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions sets a cached matrix and calculates the correspondent inverse matrix
 
-## Write a short comment describing this function
+## This function caches a Matrix and its attributes:
+## set import a matrix
+## get returns a imported matrix
+## setdet import the determinant value
+## getdet return the cached determinant velue
+## setinv import inverse matrix
+## getinv returns the cached inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		inv <- NULL
+		detm <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+                detm <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        setdet <- function(detm) detm <<- det
+        getinv <- function() inv
+        getdet <- function() detm
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv,setdet = setdet, getdet = getdet )
 }
 
 
-## Write a short comment describing this function
+## This function calculate the inverse matrix
+## the function returns the cached inverse matrix in case it has been already calculated
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+
+        data <- x$get()
+        
+        detm <- x$getdet()
+        
+        if(!is.null(detm) && detm != 0) {
+                inv <- solve(data,...)
+                x$setinv(inv)
+                return(inv)
+        }
+
+        detm <- det(data, ...)
+        x$setdet(detm)
+
+        if(detm != 0){
+
+        	inv <- solve(data,...)
+        	x$setinv(inv)
+        	return(inv)
+        }
+
+        message("Matrix is not invertible")
+        return(NULL)
 }
+
